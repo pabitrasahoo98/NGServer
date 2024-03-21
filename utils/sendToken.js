e04884a85cdc799a8d0ff3152048a3446597c629
pabitrasahoo98 <152062702+pabitrasahoo98@users.noreply.github.com>
2024-03-21 10:53:27 +0530

    send token secure2

--- utils/sendToken.js
+++ utils/sendToken.js
@@ -6,7 +6,9 @@ const sendToken=(user,statusCode,res)=>{
             Date.now()+process.env.COOKIE_EXPIRE*24*60*60*1000),
             httpOnly:true,
             secure: true,
-            path: '/'
+            path: '/',
+            domain:'vercel.app',
+            sameSite: 'none'
         
     };
     res.status(statusCode).cookie("token",token,options).json({
