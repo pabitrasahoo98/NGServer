02c6e239db14aca9c813d89250ad4f81a7702688
pabitrasahoo98 <152062702+pabitrasahoo98@users.noreply.github.com>
2024-03-22 22:59:51 +0530

    update25

--- utils/sendToken.js
+++ utils/sendToken.js
@@ -4,7 +4,6 @@ const sendToken=(user,statusCode,res)=>{
     const options={
         expires:new Date(
             Date.now()+process.env.COOKIE_EXPIRE*24*60*60*1000),
-            httpOnly:true,
             secure: true,
             path: '/',
             sameSite: 'none'
